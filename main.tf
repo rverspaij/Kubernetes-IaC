41e61d3aa6af33d31ae5ed280e9191def6d52b97
Raoul <rnb1acc.verspaij@gmail.com>
2025-05-16 16:22:56 +0200

    New Start

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">=1.4.0"
+
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">= 3.80.0"
+    }
+    kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = ">=2.27.0"
+    }
+    helm = {
+        source = "hashicorp/helm"
+        version = ">= 2.13.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
+}
+
+locals {
+  project = "demo"
+  environment = "dev"
+  location = var.location
+
+  tags = {
+    project = local.project
+    environment = local.environment
+    managed_by = "terraform"
+  }
+}
+
+module "resource_group" {
+  source = "./modules/resource_group"
+  resource_group_name = "${local.project}-${local.environment}-rg"
+  location = local.location
+  tags = local.tags
+}
--- resourceGroup.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "k8s-rg" {
-    name = "${var.username}-rg"
-    location = var.location
-}
-
-output "id" {
-    value = "data.azurerm_resource_group.k8s-rg.id"
-}
